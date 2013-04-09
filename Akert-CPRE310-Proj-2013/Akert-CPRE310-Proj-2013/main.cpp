d5a11906001aa87e003b2ab00a56f9ef4eb3f080
Karl Akert <kpakert@gmail.com>
2013-04-09 13:45:03 -0500

    Finished with an algorithm implementation
    bug fixing atm
    
    Squashed commit of the following:
    
    commit 8d799df7de3a553abc2befd4ba900248f61b5422
    Merge: 60b9869 5c1388c
    Author: Karl Akert <kpakert@gmail.com>
    Date:   Mon Apr 8 23:46:42 2013 -0500
    
        Merge branch 'develop'
    
    commit 60b98697d136289e06e4fc24d902d694f9920055
    Author: Karl Akert <kpakert@gmail.com>
    Date:   Mon Apr 8 23:45:21 2013 -0500
    
        Squashed commit of the following:
    
        commit 5c1388c85049a14f0f6c05e18b96348cb9b17ac4
        Author: Karl Akert <kpakert@gmail.com>
        Date:   Mon Apr 8 23:43:06 2013 -0500
    
            Fixed parsing
    
            Fixed parsing to account for tabs or additional spaces, also allowing for
            larger than one digit numbers.
    
        commit 227d391854a1387ccde0e2c7af81ad5557d66298
        Author: Karl Akert <kpakert@gmail.com>
        Date:   Mon Apr 8 22:41:38 2013 -0500
    
            First Milestone
    
            I am able to read in the test files!!!!
    
        commit 76f755aecc0cbd7409efb92e3cafab80a6a787a5
        Author: Karl Akert <kpakert@gmail.com>
        Date:   Tue Apr 2 22:30:37 2013 -0500
    
            Updated the readme

--- Akert-CPRE310-Proj-2013/Akert-CPRE310-Proj-2013/main.cpp
+++ Akert-CPRE310-Proj-2013/Akert-CPRE310-Proj-2013/main.cpp
@@ -1,14 +1,43 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
+#include <vector>
+#include <deque>
 using namespace std;
+
+typedef struct Page {
+	vector<int> linkTo;
+	vector<int> linkFrom;
+	double rankOld;
+	double rankNew;
+
+	Page()
+	{
+		linkTo = vector<int>();
+		linkFrom = vector<int>();
+		rankOld = 0.85;
+		rankNew = 1.0;
+	}
+} Page;
+
+void addEdge(int a, int b, deque<Page>& pages);
+bool iteration(deque<Page> &pages);
+bool contains(const std::vector<int> &vec, const int &value);
+void printRank(deque<Page> &pages);
 
 void main()
 {
+	int maxPages = 100;
 	string filename;
 	cout << "Enter a file to open:" << endl;
 	cin >> filename;
+	cout << "Enter the number of pages:" << endl;
+	cin >> maxPages;
 	//cout << filename << endl;
+
+	deque<Page> pages1(maxPages, Page());
+	deque<Page> pages2(maxPages, Page());
 	
 	ifstream input;
 	input.open(filename);
@@ -24,15 +53,102 @@ void main()
 			int a, b, i;
 			i = line.find("\t");
 			if( i == -1) i = line.find(" ");
-			cout << i << endl;
+			//cout << i << endl;
 			a = atoi(line.substr(0,i).c_str());
 			i = line.find_last_of("\t");
 			if ( i == -1) i = line.find_last_of(" ");
-			cout << i << endl;
+			//cout << i << endl;
 			b = atoi(line.substr(i + 1).c_str());
-			cout << a << " " << b << endl;
+			//cout << a << " " << b << endl;
+
+			addEdge(a,b,pages1);
+			addEdge(b,a,pages2);
+			
+			//cout << pages1[a - 1].linkTo.size() << endl;
+			//cout << pages1[b - 1].linkFrom.size() << endl;
+			//cout << a << ": " << pages1[a-1].linkTo.back() <<"	"<< b << ": " <<pages1[b-1].linkFrom.back() << endl;
+			//cout << pages2[a - 1].linkTo.size() << endl;
+			//cout << pages2[b - 1].linkFrom.size() << endl;
+			//cout << b << ": " << pages2[b-1].linkTo.back() <<"	"<< a << ": " <<pages2[a-1].linkFrom.back() << endl;
+		}
+	}
+	input.close();
+
+	bool iterate = true;
+	int iterations = 0;
+	while(iterate)
+	{
+		iterate = !iteration(pages1);
+		iterations++;
+	}
+	//cout << "completed iterating" << endl;
+	cout << "Interpretation 1, Iterations: " << iterations << endl;
+	printRank(pages1);
+
+	iterate = true;
+	iterations = 0;
+	while(iterate)
+	{
+		iterate = !iteration(pages2);
+		iterations++;
+	}
+	//cout << "completed iterating" << endl;
+	cout << "Interpretation 2, Iterations: " << iterations << endl;
+	printRank(pages2);
+}
+
+void addEdge(int a, int b, deque<Page> &pages)
+{
+	if(!contains(pages[a-1].linkTo, b) && !contains(pages[b-1].linkFrom, a))
+	{
+		pages[a - 1].linkTo.push_back(b);
+		pages[b - 1].linkFrom.push_back(a);
+	}
+}
+
+/**
+* This function runs a single iteration and returns true if no more iterations are needed
+*/
+bool iteration(deque<Page> &pages)
+{
+	bool last = true;
+	
+	// calculate pagerank
+	for (int i = 0; i < pages.size(); i++)
+	{
+		double sum = 0;
+		for (int j = 0; j < pages[i].linkFrom.size(); j ++)
+		{
+			Page temp = pages[pages[i].linkFrom[j] - 1];
+			sum += (temp.rankOld / temp.linkTo.size());
+		}
+		pages[i].rankNew = 0.85 + 0.15 * sum;
+
+		if(abs(pages[i].rankNew - pages[i].rankOld) > 0.001)
+		{
+			last = false;
 		}
 	}
 
-	input.close();
+	// Update the pagerank values from new to old
+	for (int i = 0; i < pages.size(); i++)
+	{
+		pages[i].rankOld = pages[i].rankNew;
+	}
+
+	return last;
+}
+
+bool contains(const std::vector<int> &vec, const int &value)
+{
+    return find(vec.begin(), vec.end(), value) != vec.end();
+}
+
+void printRank(deque<Page> &pages)
+{
+	for (int i = 0; i < pages.size(); i++)
+	{
+		cout << /*"Pagerank " <<*/ i + 1 << ": " << pages[i].rankNew << "\t";// << endl;
+	} 
+	cout << endl;
 }
